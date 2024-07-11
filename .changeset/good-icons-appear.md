cc8c5ebc9d2749f4e77061aca19a24ce39469434
Ludovic Muller <ludovicmuller1@gmail.com>
2024-07-11 15:00:56 +0200

    chore: export tools and types

---
+++ .changeset/good-icons-appear.md
@@ -0,0 +1,5 @@
+---
+"@ludovicm67/simple-whiteboard": patch
+---
+
+Export tools and types
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,21 @@
 import { SimpleWhiteboard } from "./simple-whiteboard";
+import SimpleWhiteboardTool from "./lib/SimpleWhiteboardTool";
+export type {
+  BoundingRect,
+  RoughCanvas,
+  RoughCanvasOptions,
+  SimpleWhiteboardToolInterface,
+  WhiteboardItem,
+} from "./lib/SimpleWhiteboardTool";
+import { SimpleWhiteboardToolCircle } from "./tools/circle";
+import { SimpleWhiteboardToolClear } from "./tools/clear";
+import { SimpleWhiteboardToolDefaults } from "./tools/defaults";
+import { SimpleWhiteboardToolLine } from "./tools/line";
+import { SimpleWhiteboardToolMove } from "./tools/move";
+import { SimpleWhiteboardToolPen } from "./tools/pen";
+import { SimpleWhiteboardToolPicture } from "./tools/picture";
+import { SimpleWhiteboardToolPointer } from "./tools/pointer";
+import { SimpleWhiteboardToolRect } from "./tools/rect";
 
 declare global {
   interface HTMLElementTagNameMap {
@@ -6,5 +23,17 @@ declare global {
   }
 }
 
-export { SimpleWhiteboard };
+export {
+  SimpleWhiteboard,
+  SimpleWhiteboardTool,
+  SimpleWhiteboardToolCircle,
+  SimpleWhiteboardToolClear,
+  SimpleWhiteboardToolDefaults,
+  SimpleWhiteboardToolLine,
+  SimpleWhiteboardToolMove,
+  SimpleWhiteboardToolPen,
+  SimpleWhiteboardToolPicture,
+  SimpleWhiteboardToolPointer,
+  SimpleWhiteboardToolRect,
+};
 export default SimpleWhiteboard;
