e23b27cd666c7c9db0c60c829c95fe673a321090
Ludovic Muller <ludovicmuller1@gmail.com>
2025-03-14 15:38:24 +0100

    refactor: move main styles out

--- src/simple-whiteboard.ts
+++ src/simple-whiteboard.ts
@@ -23,6 +23,7 @@ import {
 } from "./lib/item";
 import { DrawingContext } from "./lib/types";
 import { CoordsContext } from "./lib/coords";
+import { styles } from "./styles";
 
 type Point = {
   x: number;
@@ -76,135 +77,7 @@ export class SimpleWhiteboard extends LitElement {
   @state() private selectedItemId: string | null = null;
   @state() private hoveredItemId: string | null = null;
 
-  static styles = css`
-    .root {
-      height: 100%;
-      width: 100%;
-      background-color: #fcfcff;
-      position: relative;
-    }
-
-    .button {
-      background-color: rgba(0, 0, 0, 0.05);
-      border-radius: 8px;
-      padding: 8px;
-      border: 1px solid rgba(0, 0, 0, 0.1);
-      cursor: pointer;
-    }
-    .button:hover {
-      background-color: rgba(0, 0, 0, 0.1);
-    }
-
-    .width-100-percent {
-      width: 100%;
-    }
-
-    .menu {
-      position: absolute;
-      z-index: 2;
-      top: 16px;
-      left: 16px;
-      user-select: none;
-    }
-
-    .menu button:hover {
-      background-color: #dfdfdf;
-    }
-
-    .tools {
-      user-select: none;
-      gap: 8px;
-      padding: 3px;
-      border-radius: 8px;
-      background-color: #fff;
-      margin: auto;
-      position: absolute;
-      z-index: 1;
-      top: 16px;
-      left: 50%;
-      transform: translateX(-50%);
-      box-shadow: 0 0 8px rgba(0, 0, 0, 0.1);
-      overflow-x: auto;
-      white-space: nowrap;
-      max-width: calc(100% - 128px);
-      scrollbar-width: thin;
-    }
-
-    .tools button {
-      background-color: transparent;
-      color: #000;
-      border: none;
-      cursor: pointer;
-      padding: 8px;
-      border-radius: 4px;
-      transition: background-color 0.2s;
-    }
-
-    .tools button:hover {
-      background-color: rgba(0, 0, 0, 0.05);
-    }
-
-    .tools button:active,
-    .tools .tools--active {
-      background-color: rgba(0, 0, 0, 0.1);
-    }
-
-    .tools-options {
-      user-select: none;
-      position: absolute;
-      z-index: 1;
-      box-shadow: 0 0 8px rgba(0, 0, 0, 0.1);
-      top: 84px;
-      width: 200px;
-      left: 16px;
-      background-color: #fff;
-      border-radius: 8px;
-      padding: 8px 12px;
-    }
-
-    .footer-tools {
-      position: absolute;
-      z-index: 1;
-      bottom: 0;
-      left: 0;
-      background-color: #f2f3f3;
-      padding: 2px 8px;
-      border-top-right-radius: 8px;
-      box-shadow: 0 0 8px rgba(0, 0, 0, 0.1);
-      display: flex;
-      justify-content: space-between;
-      flex-direction: row;
-      align-items: center;
-      gap: 8px;
-      color: rgba(0, 0, 0, 0.5);
-    }
-
-    .footer-tools select {
-      color: rgba(0, 0, 0, 0.5);
-      padding: 4px;
-    }
-
-    @media (max-width: 450px) {
-      .tools-options {
-        width: calc(100% - 64px);
-      }
-    }
-
-    .tools-options p {
-      font-size: 14px;
-      margin: 0;
-    }
-
-    canvas {
-      top: 0;
-      left: 0;
-      position: absolute;
-      right: 0;
-      bottom: 0;
-      width: 100%;
-      height: 100%;
-    }
-  `;
+  static styles = styles;
 
   protected firstUpdated(): void {
     this.canvas = this.shadowRoot?.querySelector("canvas") || undefined;
